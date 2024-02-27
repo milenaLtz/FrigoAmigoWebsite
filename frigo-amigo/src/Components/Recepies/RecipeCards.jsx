daab17acc236d4a9eefe6323a4cedac7687ed0e8
milenaLtz <milena.letz@gmail.com>
2024-02-27 13:22:40 +0200

    deleted commented code

--- frigo-amigo/src/Components/Recepies/RecipeCards.jsx
+++ frigo-amigo/src/Components/Recepies/RecipeCards.jsx
@@ -39,67 +39,6 @@ const RecipeCards = (props) => {
             </li>
           );
         })}
-        {/* <li>
-                <div className="recipe-card" >
-                    <img className="recipe-image" src={soup} alt="soup"></img>
-                    <img className="recipe-arrow" src={arrows} alt="arrows"></img>
-                    <div className="recipe-heading nextart-700">Сырный суп для Милены</div>
-                    <img className="recipe-line" src={line} alt="line"></img>
-                    <div className="recipe-ingredients roundmplus-600">
-                        Ингредиенты:
-                    </div>
-                    <div className="recipe-ingredients-items roundmplus-600">
-                        <ul style={{listStyleType: "disc"}}>
-                            <li>Кокейн по вкусу</li>
-                            <li>Тесто вот это да!</li>
-                            <li>Дети куриные</li>
-                            <li>Протеин чтобы пп</li>
-                        </ul>
-                    </div>
-                    <div className="recipe-pfc-per-100 roundmplus-600">БЖУ на 100г</div>
-                    <div className="recipe-pfc-calories roundmplus-600">Б 0г |  Ж 0г  |  У 0г  |  0кк</div>
-                </div>
-            </li> 
-            
-                {
-    "id": "1",
-    "firstName": "Milena", 
-    "lastName": "Letz",
-    "phone": "0982345681",
-    "email": "letz@dot.com",
-  },
-  
-  {
-    "id": "2",
-    "firstName": "Lisa", 
-    "lastName": "Ekshina",
-    "phone": "0982306723",
-    "email": "ekshina@dot.com",
-  },
-  
-  {
-    "id": "3",
-    "firstName": "Vanja", 
-    "lastName": "Kusminykh",
-    "phone": "0982398256",
-    "email": "kusminykh@dot.com",
-  },
-  
-  {
-    "id": "4",
-    "firstName": "Zahar", 
-    "lastName": "Rudenko",
-    "phone": "0982367823",
-    "email": "rudenko@dot.com",
-  },
-  
-  {
-    "id": "5",
-    "firstName": "Olja", 
-    "lastName": "Efimova",
-    "phone": "0982309357",
-    "email": "efimova@dot.com",
-  }*/}
       </ul>
       <div className="recipe-button">
         <button className="recipe-button-design manrope-400">Смотреть больше рецептов</button>
